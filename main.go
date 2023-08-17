0ca25d5f9e36e001858e7b8da892c3cf5c566db7
Kizitora3000 <kutaku2000@gmail.com>
2023-08-17 16:53:55 +0900

    add: SecureActionSelection

--- main.go
+++ main.go
@@ -8,35 +8,65 @@ import (
 	"pprlgo/party"
 
 	"github.com/tuneinsight/lattigo/v4/ckks"
+	"github.com/tuneinsight/lattigo/v4/rlwe"
 )
 
-var user party.User = party.User{
-	Actions: []float64{0.1, 0.2, 0.3, 0.4},
-	States:  []float64{1.0, 2.0, 3.0},
-	Alpha:   0.5,
-	Gamma:   0.9,
-	Qtable: [][]float64{
-		{0.1, 0.2, 0.3, 0.4},
-		{0.5, 0.6, 0.7, 0.8},
-		{0.9, 1.0, 1.1, 1.2},
-	},
+var (
+	user party.User = party.User{
+		Actions: []float64{0.1, 0.2, 0.3, 0.4},
+		States:  []float64{1.0, 2.0, 3.0},
+		Alpha:   0.5,
+		Gamma:   0.9,
+		Qtable: [][]float64{
+			{0.1, 0.2, 0.3, 0.4},
+			{0.5, 0.6, 0.7, 0.8},
+			{0.9, 1.0, 1.1, 1.2},
+		},
+	}
+	cloud_platform party.CloudPlatform = party.CloudPlatform{
+		Qtable: [][]float64{
+			{0.1, 0.2, 0.3, 0.4},
+			{0.5, 0.6, 0.7, 0.8},
+			{0.9, 1.0, 1.1, 1.2},
+		},
+	}
+	EncryptedQtable []*rlwe.Ciphertext
+)
+
+func SecureQtableUpdating(params ckks.Parameters, encoder ckks.Encoder, encryptor rlwe.Encryptor, decryptor rlwe.Decryptor, evaluator ckks.Evaluator, publicKey *rsa.PublicKey, privateKey *rsa.PrivateKey, v_t []float64, w_t []float64, Q_new []float64) *rlwe.Ciphertext {
+	
 }
 
-var cloud_platform party.CloudPlatform = party.CloudPlatform{
-	Qtable: [][]float64{
-		{0.1, 0.2, 0.3, 0.4},
-		{0.5, 0.6, 0.7, 0.8},
-		{0.9, 1.0, 1.1, 1.2},
-	},
-}
+func SecureActionSelection(params ckks.Parameters, encoder ckks.Encoder, encryptor rlwe.Encryptor, decryptor rlwe.Decryptor, evaluator ckks.Evaluator, publicKey *rsa.PublicKey, privateKey *rsa.PrivateKey, v_t []float64) *rlwe.Ciphertext {
+	VtName := "VtName"
 
-func SecureActionSelection(u party.User, cp party.CloudPlatform) {
-	for j := 0; j < len(u.Actions); j++ {
-		for i := 0; i < len(u.States); i++ {
-
+	for i := 0; i < len(cloud_platform.Qtable); i++ {
+		filename := fmt.Sprintf(VtName+"_%d", i)
+		if v_t[i] == 0 {
+			zeros := make([]float64, len(cloud_platform.Qtable[i]))
+			doublenc.DEenc(params, encoder, encryptor, publicKey, zeros, filename)
+		} else if v_t[i] == 1 {
+			ones := make([]float64, len(cloud_platform.Qtable[i]))
+			for i := range ones {
+				ones[i] = 1
+			}
+			doublenc.DEenc(params, encoder, encryptor, publicKey, ones, filename)
 		}
 	}
-	return
+
+	zeros := make([]float64, len(cloud_platform.Qtable[0]))
+	result := doublenc.FHEenc(params, encoder, encryptor, zeros)
+	for i := 0; i < len(cloud_platform.Qtable); i++ {
+		filename := fmt.Sprintf(VtName+"_%d", i)
+		vt := doublenc.RSAdec(privateKey, filename)
+		evaluator.Mul(vt, EncryptedQtable[i], vt)
+
+		// The multiplicable depth is one, so Relinearize is used to reset depth.
+		evaluator.Relinearize(vt, vt)
+		evaluator.Add(result, vt, result)
+	}
+
+	return result
 }
 
 func main() {
@@ -55,34 +85,31 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
 	kgen := ckks.NewKeyGenerator(params)
 	sk := kgen.GenSecretKey()
 	encryptor := ckks.NewEncryptor(params, sk)
 	decryptor := ckks.NewDecryptor(params, sk)
 	encoder := ckks.NewEncoder(params)
-
+	rlk := kgen.GenRelinearizationKey(sk, 1)
+	evaluator := ckks.NewEvaluator(params, rlwe.EvaluationKey{Rlk: rlk})
 	privateKey, _ := rsa.GenerateKey(rand.Reader, 2048)
 	publicKey := &privateKey.PublicKey
 
-	/*
-		v_t := []float64{0, 1.0, 0}
-		c_v_t := doublenc.FHEenc(params, encoder, encryptor, v_t)
-		p_v_t := doublenc.FHEdec(params, encoder, decryptor, c_v_t)
+	// encrypt Qtalbe in CP
+	var encrypted_Qtable []*rlwe.Ciphertext
+	for _, row := range cloud_platform.Qtable {
+		ciphertext := doublenc.FHEenc(params, encoder, encryptor, row)
+		encrypted_Qtable = append(encrypted_Qtable, ciphertext)
+	}
+	EncryptedQtable = encrypted_Qtable
 
-		fmt.Println(p_v_t)
+	v_t := []float64{0, 1, 0}
+	res1 := SecureActionSelection(params, encoder, encryptor, decryptor, evaluator, publicKey, privateKey, v_t)
+	fmt.Println(doublenc.FHEdec(params, encoder, decryptor, res1))
 
-		doublenc.RSAenc(publicKey, c_v_t, "cvt")
-		hoge := doublenc.RSAdec(privateKey, "cvt")
-		valuesTest := encoder.Decode(decryptor.DecryptNew(hoge), params.LogSlots())
-		fmt.Printf("ValuesTest: %6.10f %6.10f %6.10f %6.10f...\n", valuesTest[0], valuesTest[1], valuesTest[2], valuesTest[3])
-	*/
+	w_t := []float64{0, 0, 1, 0}
+	Q_new := []float64{3.5}
 
-	v_t := []float64{0, 1.0, 0}
-	doublenc.DEenc(params, encoder, encryptor, publicKey, v_t, "cvt")
-	valuesTest := doublenc.DEdec(params, encoder, decryptor, privateKey, "cvt")
-	fmt.Printf("ValuesTest: %6.10f %6.10f %6.10f %6.10f...\n", valuesTest[0], valuesTest[1], valuesTest[2], valuesTest[3])
-
-	abc := doublenc.FHEenc(params, encoder, encryptor, cloud_platform.Qtable[0])
-	bc := doublenc.FHEdec(params, encoder, decryptor, abc)
-	fmt.Println(bc)
+	res2 := SecureQtableUpdating(params, encoder, encryptor, decryptor, evaluator, publicKey, privateKey, v_t, w_t, Q_new)
 }
